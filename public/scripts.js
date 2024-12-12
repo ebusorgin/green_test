ae1298ea8e2a630f140e3e834cf62be2e4877ac1
severomorets <severomorets@gmail.com>
2024-12-12 11:36:03 +0100

    fix url

--- public/scripts.js
+++ public/scripts.js
@@ -213,7 +213,7 @@ class Page {
     }
     async http(method, body={}, type = 'POST') {
         let url = `https://api.green-api.com`
-        this.currentInstance.type==='green'?null:url = `https://green.xyyzzz.ru:4000`;
+        this.currentInstance.type==='green'?null:url = `https://green.xyyzzz.ru/api`;
 
         const fetchOptions = {
             method: type,
